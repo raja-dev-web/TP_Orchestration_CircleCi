6f3539aa7ac9a4223d4b0e1b764793e18e9c10c7
raja-dev-web <84727110+raja-dev-web@users.noreply.github.com>
2022-03-24 15:18:00 +0100

    Delete evenodd.py

--- evenodd.py
+++
@@ -1,9 +0,0 @@
-def evenorodd(num):
-	if (num % 2) == 0:  # this is updated
-		return "Even Number"  
-	else:  
-		return "Odd Number" 
-
-if __name__ == '__main__':
-    evenorodd(num)
-# this is updated

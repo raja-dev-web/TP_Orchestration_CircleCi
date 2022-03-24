d6d4831dd28c2ab99cdc15124c911ed0e13c11f9
raja-dev-web <raja.web.dev11@gmail.com>
2022-03-24 12:58:58 +0000

    python function

---
+++ evenodd.py
@@ -0,0 +1,11 @@
+def evenorodd(num):
+    if (num % 2) == 0:  
+	return "Even Number"  
+    else:  
+	return "Odd Number" 
+		
+def mainFunc():
+    print("This is the main function")
+		
+if __name__ == '__main__':
+    mainFunc()
---
+++ testevenodd.py
@@ -0,0 +1,8 @@
+from evenodd import evenorodd
+
+def TestEvenorOdd():
+    assert evenorodd(6) == "Even Number"
+    print("Execution Success!!!")
+
+if __name__ == '__main__':
+    TestEvenofOdd()
